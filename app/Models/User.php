cfa70c2dec53ed456eba3775d1ca833044104a2b
Ken Arceo <johnarbyarceo@gmail.com>
2022-03-15 01:50:24 +0800

    remove hidden csrf and api token

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'api_token', 'csrf_token',
+        'password', 'remember_token',
     ];
     /**
      * Roll API Key
