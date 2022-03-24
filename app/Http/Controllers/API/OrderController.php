e8e6d9b7d1e080e0092612d72ce330dbb3f549cb
Ken Arceo <johnarbyarceo@gmail.com>
2022-03-25 05:21:42 +0800

    api for orders

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\Order;
+use App\Models\OrderAddress;
 use App\Models\OrderTracker;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -15,25 +16,27 @@ class OrderController extends APIBaseController
         try {
             DB::beginTransaction();
             $orderNo = mt_rand(100000000, 999999999);
-            $query = new Order();
-            $query->number = $orderNo;
-            $query->user_id = $request->get('user_id');
-            $query->item_id = $request->get('item_id');
-            $query->quantity = $request->get('quantity');
-
-            // $query->user_address_id = $request->get('user_address_id');
-            $query->save();
-
-            /* $item = new OrderItem();
-            $item->order_id = $query->id;
-            $item->user_id = $request->get('user_id');
-            $item->food_id = $request->get('food_id');
-            $item->qty = $request->get('qty');
-            $item->save();
+            foreach ($request->get('cart') as $key => $value) {
+                $query = new Order();
+                $query->number = $orderNo;
+                $query->user_id = $request->get('user_id');
+                $query->item_id = $value['id'];
+                $query->quantity = $value['qty'];
+                $query->save();
+            }
+            $address = new OrderAddress();
+            $address->user_id = $request->get('user_id');
+            $address->order_number = $orderNo;
+            $address->no = $request->get('no');
+            $address->street = $request->get('street');
+            $address->area = $request->get('area');
+            $address->city = $request->get('city');
+            $address->longitude = $request->get('longitude');
+            $address->latitude = $request->get('latitude');
+            $address->save();
             $tracker = new OrderTracker();
-            $tracker->order_id = $query->id;
-            $tracker->status = 1;
-            $tracker->save(); */
+            $tracker->order_id = $orderNo;
+            $tracker->save();
             DB::commit();
             return $this->sendResponse($query, 'Success.');
         } catch (\Exception$e) {
